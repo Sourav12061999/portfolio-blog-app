21df012f9d417cd90cfa39f84881cdf64c6cdbdc
Sourav12061999 <sdunity12061999@gmail.com>
2023-01-04 08:58:17 +0530

    Schema Export Complete

---
+++ Schemas/index.ts
@@ -0,0 +1,14 @@
+// Models Export
+import { cardModel } from "./card";
+import { blogModel } from "./blog";
+export { cardModel, blogModel };
+
+// Mongoose Type
+import { CardTypesMongose } from "./card";
+import { BlogTypesMongoose } from "./blog";
+export type { CardTypesMongose, BlogTypesMongoose };
+
+// Schema Type
+import { CardTypes } from "./card";
+import { BlogTypes } from "./blog";
+export type { CardTypes, BlogTypes };
